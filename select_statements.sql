ec73bf0f6be423ab61128527440e7dba6db44562
Adithya <104718482+Adithyabathineni@users.noreply.github.com>
2025-02-05 21:57:49 -0500

    Updated.sql

--- select_statements.sql
+++ select_statements.sql
@@ -80,5 +80,66 @@ WHERE
     (item_price - discount_amount) * quantity > 500
 ORDER BY 
     item_total DESC;
+/*------------------------------------2-----------------------------------
+Write a SELECT statement that returns one row for each
+category that has products with these columns:
+-The category_name column from the Categories table 
+-The count of the products in the Products table 
+-The list price of the most expensive product in the Products table
+Sort the result set so the category with the most products appears first.
+------------------------------------------------------------------------*/
 
+SELECT 
+    c.category_name,
+    COUNT(p.product_id) AS count,
+    MAX(p.list_price)
+FROM
+    categories AS c
+        INNER JOIN
+    products p ON c.category_id = p.category_id
+GROUP BY c.category_name
+ORDER BY count DESC;
+/*------------------------------------7-----------------------------------
+Write a SELECT statement that answers this question: Which customers have ordered more than
+one product? Return these columns:
+-The email address from the Customers table
+-The count of distinct products from the customer’s orders
+------------------------------------------------------------------------*/
+SELECT 
+    c.email_address, COUNT(DISTINCT oi.product_id) AS count
+FROM
+    customers AS c
+        INNER JOIN
+    orders o ON c.customer_id = o.customer_id
+        INNER JOIN
+    order_items oi ON o.order_id = oi.order_id
+GROUP BY c.email_address
+HAVING count > 1;
+Write a SELECT statement that returns one row for each customer that 
+has orders with these columns:
+-The email_address column from the Customers table
+-The sum of the item price in the Order_Items table multiplied by the quantity in the Order_Items table
+-The sum of the discount amount column in the Order_Items table multiplied by the quantity in the Order_Items table
+Sort the result set in descending sequence by the item price total for each customer.
+------------------------------------------------------------------------*/
+/*------------------------------------3-----------------------------------
+Write a SELECT statement that returns one row for each customer that 
+has orders with these columns:
+-The email_address column from the Customers table
+-The sum of the item price in the Order_Items table multiplied by the quantity in the Order_Items table
+-The sum of the discount amount column in the Order_Items table multiplied by the quantity in the Order_Items table
+Sort the result set in descending sequence by the item price total for each customer.
+------------------------------------------------------------------------*/
+SELECT 
+    c.email_address,
+    SUM(o.item_price) * COUNT(o.item_id) AS first_sum_by_count,
+    SUM(o.discount_amount) * COUNT(o.item_id) AS second_sum_by_count
+FROM
+    customers AS c
+        INNER JOIN
+    orders AS ord ON c.customer_id = ord.customer_id
+        INNER JOIN
+    order_items AS o ON o.order_id = ord.order_id
+GROUP BY c.customer_id
+ORDER BY SUM(o.item_price) DESC;
 
